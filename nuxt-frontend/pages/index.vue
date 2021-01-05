595c1d8e79f3edde183e53db45a32645d97a02b9
unknown <padippurakkalminhaj@gmail.com>
2021-01-05 14:48:34 +0530

    Nuxt setup

---
+++ nuxt-frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Souq</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default { head: { title: 'Souq' } }
+</script>
+
+<style>
+h1 {
+  color: rebeccapurple;
+}
+</style>
